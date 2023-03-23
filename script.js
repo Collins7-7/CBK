5c4e40e288513703cbcd222d5d0744223bbe3a64
COllins <collinsmuiruri77@gmail.com>
2023-03-23 22:44:21 +0300

    Added menu fading animation

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const tabs =  document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent  = document.querySelectorAll('.operations__content');
+
+const nav = document.querySelector('.nav');
 
 
 const openModal = function (e) {
@@ -85,14 +90,11 @@ document.querySelector('.nav__links').addEventListener("click", function(e){
  const id =  e.target.getAttribute('href')
  document.querySelector(id).scrollIntoView({behavior: 'smooth'})
   };
-})
+});
 
 ////////////////////////////////////////////////////////////////
 //Tabbed Component
 ////////////////////////////////////////////////////////////////
-const tabs =  document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent  = document.querySelectorAll('.operations__content');
 
 
 tabsContainer.addEventListener("click", function(e){
@@ -108,4 +110,24 @@ clicked.classList.add('operations__tab--active');
 tabsContent.forEach(c => c.classList.remove('operations__content--active'));
 //Activate content area
 document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
-})
+});
+/////////////////////////////////////////////////////////
+//Menu fade animation
+////////////////////////////////////////////////////////
+
+const handleHover = function(e){
+  if(e.target.classList.contains('nav__link')){
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+  
+    siblings.forEach(el => {
+      if(el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+
+nav.addEventListener("mouseout", handleHover.bind(1));
